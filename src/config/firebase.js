dfb03d5c8f4b5576c8234ad4bdad5830f13c5272
evandrolaguna08 <67765671+evandrolaguna08@users.noreply.github.com>
2020-07-03 00:22:29 -0300

    Update firebase.js

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -2,14 +2,14 @@ import * as firebase from 'firebase';
 import 'firebase/firestore';
 
 var firebaseConfig = {
-    apiKey: "AIzaSyBlDRG0mfCg4ynSjZ2bdETid-8EIp3QxE8",
-    authDomain: "prova-c36da.firebaseapp.com",
-    databaseURL: "https://prova-c36da.firebaseio.com",
-    projectId: "prova-c36da",
-    storageBucket: "prova-c36da.appspot.com",
-    messagingSenderId: "1095774557497",
-    appId: "1:1095774557497:web:c74872198d2957330e04e8",
-    measurementId: "G-BJSCFT8XP9"
+    apiKey: "sfuhuasgagusd7yqwgggsga",
+    authDomain: "provaVinicius-c36da.firebaseapp.com",
+    databaseURL: "https://provaVinicius-c36da.firebaseio.com",
+    projectId: "provaVinicius-c36da",
+    storageBucket: "provaVinicius-c36da.appspot.com",
+    messagingSenderId: "10957745574555S97",
+    appId: "1:109de5v7ds74557497:web:c74872sa1df98d29sdf57330e04e8",
+    measurementId: "G-BJSCFT8XPASD559"
 };
 
 // Initialize Firebase
